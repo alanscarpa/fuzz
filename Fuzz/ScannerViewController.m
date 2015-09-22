9657555764d3e3f1923d1ffa2a004638b2db8a7d
Alan Scarpa <alan.scarpa@gmail.com>
2015-09-22 13:47:18 -0400

    Initial commit

---
+++ Fuzz/ScannerViewController.m
@@ -0,0 +1,132 @@
+/**
+ * Copyright (c) 2014 Moodstocks SAS
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#import "ScannerViewController.h"
+
+static int kMSResultTypes = MSResultTypeImage  |
+                            MSResultTypeQRCode |
+                            MSResultTypeEAN13;
+
+#define MS_API_KEY    @"cgxz7hy55uckm98aqz6a"
+#define MS_API_SECRET @"dQIIY4amYf2Zh4YP"
+
+@interface ScannerViewController () <MSAutoScannerSessionDelegate>
+
+@property (weak, nonatomic) IBOutlet UIView *scanBounds;
+@property (strong, nonatomic) IBOutlet UIView *videoPreview;
+
+@end
+
+@implementation ScannerViewController {
+    MSAutoScannerSession *_scannerSession;
+}
+
+
+- (void)dealloc
+{
+    [_scannerSession stopRunning];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.scanBounds.backgroundColor = [UIColor clearColor];
+    self.scanBounds.layer.borderWidth = 3.0f;
+    self.scanBounds.layer.borderColor = [UIColor redColor].CGColor;
+    
+    [self initializeScanner];
+    _scannerSession = [[MSAutoScannerSession alloc] initWithScanner:_scanner];
+    _scannerSession.delegate = self;
+    _scannerSession.resultTypes = kMSResultTypes;
+    
+    CALayer *videoPreviewLayer = [self.videoPreview layer];
+    [videoPreviewLayer setMasksToBounds:YES];
+    
+    CALayer *captureLayer = [_scannerSession captureLayer];
+    [captureLayer setFrame:[self.videoPreview bounds]];
+    
+    [videoPreviewLayer insertSublayer:captureLayer
+                                below:[[videoPreviewLayer sublayers] objectAtIndex:0]];
+    
+    [_scannerSession startRunning];
+}
+
+
+
+-(void)initializeScanner
+{
+    NSString *path = [MSScanner cachesPathFor:@"scanner.db"];
+    _scanner = [[MSScanner alloc] init];
+    [_scanner openWithPath:path key:MS_API_KEY secret:MS_API_SECRET error:nil];
+    
+    // Create the progression and completion blocks:
+    void (^completionBlock)(MSSync *, NSError *) = ^(MSSync *op, NSError *error) {
+        if (error)
+            NSLog(@"Sync failed with error: %@", [error ms_message]);
+        else
+            NSLog(@"Sync succeeded (%li images(s))", (long)[_scanner count:nil]);
+    };
+    
+    void (^progressionBlock)(NSInteger) = ^(NSInteger percent) {
+        NSLog(@"Sync progressing: %li%%", (long)percent);
+    };
+    
+    // Launch the synchronization
+    [_scanner syncInBackgroundWithBlock:completionBlock progressBlock:progressionBlock];
+    self.scanner = _scanner;
+}
+
+
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - MSAutoScannerSessionDelegate
+
+- (void)session:(id)scannerSession didFindResult:(MSResult *)result
+{
+    NSString *title = [result type] == MSResultTypeImage ? @"Image" : @"Barcode";
+    NSString *message = [NSString stringWithFormat:@"%@:\n%@", title, [result string]];
+    
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+         [_scannerSession resumeProcessing];
+    }];
+    
+    [alert addAction:ok];
+    [self presentViewController:alert animated:YES completion:nil];
+    
+}
+
+
+
+
+
+@end
---
+++ Fuzz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloMS
+//
+//  Created by Cédric Deltheil on 24/02/2014.
+//  Copyright (c) 2014 Moodstocks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
